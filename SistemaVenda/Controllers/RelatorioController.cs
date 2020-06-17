7c5c62298d916764785c16b63fd64e3eb32daa20
caioh <caioh@LAPTOP-1HN4FG1J>
2020-06-17 19:29:18 -0300

    graficos

---
+++ SistemaVenda/Controllers/RelatorioController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Internal;
+using SistemaVenda.DAL;
+using SistemaVenda.Models;
+
+namespace SistemaVenda.Controllers
+{
+    public class RelatorioController : Controller
+    {
+        protected ApplicationDbContext _dbContext;
+        public RelatorioController(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public IActionResult Grafico()
+        {
+            try
+            {
+                var lista = _dbContext.VendaProdutos
+              .GroupBy(key => key.CodigoProduto)
+              .Select(
+                  obj => new GraficoViewModel
+                  {
+                      CodigoProduto = obj.First().CodigoProduto,
+                      Descricao = obj.First().Produto.Descricao,
+                      TotalVendido = obj.Sum(x => x.Quantidade)
+                  }
+              ).ToList();
+            }
+            catch (InvalidOperationException e)
+            {
+                Console.WriteLine(e.);
+            }
+           
+
+            return View();
+        }
+    }
+}
--- SistemaVenda/Entities/VendaProdutos.cs
+++ SistemaVenda/Entities/VendaProdutos.cs
@@ -9,9 +9,9 @@ namespace SistemaVenda.Entities
 {
     public class VendaProdutos
     {
-        public int? CodigoVenda { get; set; }
-        public int? CodigoProduto { get; set; }
-        public float Quantidade { get; set; }
+        public int CodigoVenda { get; set; }
+        public int CodigoProduto { get; set; }
+        public double Quantidade { get; set; }
         public decimal ValorUnitario { get; set; }
         public decimal ValorTotal { get; set; }
         public Venda Venda { get; set; }
